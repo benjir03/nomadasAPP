f49824f5bcb8b4f31d622e478c798490b2cee6bc
dani8465168 <cmendezp2100@alumno.ipn.mx>
2024-10-29 21:43:27 -0600

    header

--- src/componentes/BarraNav.jsx
+++ src/componentes/BarraNav.jsx
@@ -21,7 +21,7 @@ const BarraNav = () => {
             <a href="#services">Historias</a>
           </li>
           <li>
-            <a href="#contact">Contacto</a>
+            <a href="#contact">Contactofdgahdsg</a>
           </li>
         </ul>
       </nav>
