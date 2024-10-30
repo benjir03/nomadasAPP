9502a519c2543f5b84effc585bf62384d26c6572
AuroraCarbajal03 <icarbajalm2000@alumno.ipn.mx>
2024-10-29 21:48:10 -0600

    Describe lo que hiciste

--- src/componentes/BarraNav.jsx
+++ src/componentes/BarraNav.jsx
@@ -21,7 +21,7 @@ const BarraNav = () => {
             <a href="#services">Historias</a>
           </li>
           <li>
-            <a href="#contact">Contacto</a>
+            <a href="#contact">Contactooooooooo</a>
           </li>
         </ul>
       </nav>
