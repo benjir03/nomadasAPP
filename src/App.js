9b8e70c4d4bc1fe78d3abb43070b4ee904bcb510
04YahirAVMN <yavendanom2000@alumno.ipn.mx>
2024-11-04 10:35:14 -0600

    quite bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import BarraNav from "./componentes/BarraNav";
 import Registro from "./views/Registro";
 import InicioSesion from "./views/InicioSesion";
 import Destino from "./views/Destino";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
