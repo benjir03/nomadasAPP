4cf18ea543237ef94e5e97fdc77f6d3ae967db63
UriAntlc <ulomasc1900@alumno.ipn.mx>
2024-11-04 19:10:33 -0600

    Paso de componentes y organizacion

---
+++ src/componentes/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../estilos/styGenBut.css";
+
+const Button = () => {
+    return (
+        <div className="GenBut">
+            <button className="button">
+                Boton
+            </button>
+        </div>
+    );
+};
+
+export default Button;
