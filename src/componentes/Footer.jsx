9bbd1ead06a2411a9df3abb015a00c1435d31b12
UriAntlc <ulomasc1900@alumno.ipn.mx>
2024-11-08 17:23:05 -0600

    Correcion footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer style={{ width:'97%' ,backgroundColor: '#f1f1f1', padding: '20px', textAlign: 'center', marginTop: '20px' , position:'absolute', bottom:'0', left:'0'}}>
+    <footer style={{backgroundColor: '#f1f1f1', padding: '20px', textAlign: 'center', marginTop: '20px',}}>
       <p>&copy; {new Date().getFullYear()} Nómadas. Todos los derechos reservados.</p>
       <div style={{ marginTop: '10px' }}>
         <a href="/about" style={{ margin: '0 10px', color: '#007bff', textDecoration: 'none' }}>Acerca de</a>
