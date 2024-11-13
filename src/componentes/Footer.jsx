202c158a69b46520bb8b28e30d5a17a5b6b006da
JxsueRam <ramirezjosueeleazar@gmail.com>
2024-11-13 09:29:40 -0600

    Footer y coreccion en componenete actividad

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-
 function Footer() {
   return (
     <footer style={{ backgroundColor: '#f1f1f1', padding: '20px', textAlign: 'center', marginTop: '20px' }}>
-      <div style={{ width: '80%', margin: '0 auto' }}> {/* Ajusta el ancho del contenido */}
+      <div style={{ width: '80%', margin: '0 auto' }}>
         <p>&copy; {new Date().getFullYear()} Nómadas. Todos los derechos reservados.</p>
-        <div style={{ marginTop: '10px' }}>
+        <p>Nómadas y el logo de NómadasApp son marcas registradas o marcas comerciales de Nómadas, Inc.</p>
+
+       
+
+        {/* Social Media */}
+        <div style={{ marginTop: '20px' }}>
+          <h4>Social Media</h4>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
+            <img src={require('../imgs/facebook.png')} alt="Facebook" style={{ width: '24px', height: '24px' }} />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
+            <img src={require('../imgs/twitter.png')} alt="Twitter" style={{ width: '24px', height: '24px' }} />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
+            <img src={require('../imgs/instagram.png')} alt="Instagram" style={{ width: '24px', height: '24px' }} />
+          </a>
+        </div>
+         {/* Enlaces de navegación */}
+         <div style={{ marginTop: '10px' }}>
           <a href="/about" style={{ margin: '0 10px', color: '#007bff', textDecoration: 'none' }}>Acerca de</a>
           <a href="/contact" style={{ margin: '0 10px', color: '#007bff', textDecoration: 'none' }}>Contacto</a>
           <a href="/privacy" style={{ margin: '0 10px', color: '#007bff', textDecoration: 'none' }}>Política de Privacidad</a>
-        </div> {/* Cierre de div agregado */}
-      </div> {/* Cierre de div ajustado */}
+        </div>
+      </div>
     </footer>
   );
 }
