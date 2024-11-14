81487e86a00228f9ecd3dd0c60cd9b6f84660d65
mumur05 <epedrozav1900@alumno.ipn.mx>
2024-11-14 08:37:33 -0600

    aronow

--- src/views/ArmarPlan.jsx
+++ src/views/ArmarPlan.jsx
@@ -9,7 +9,7 @@ export default function ArmarPlan() {
   const [ciudad, setCiudad] = useState("");
   const [estancia, setEstancia] = useState("Unas cuantas horas");
   const [acompanantes, setAcompanantes] = useState("Voy solo");
-  const [presupuesto, setPresupuesto] = useState(4)
+  const [presupuesto, setPresupuesto] = useState(2)
   const handleExploraActividades = () => {
     navigate("/LugaresCarrusel", {
       state: { ciudad, estancia, acompanantes, presupuesto },
