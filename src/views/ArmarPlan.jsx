9c3dddb93e315517a27626e14be5220b885fdb29
benjir03 <joschiki.03crus@gmail.com>
2024-11-24 22:52:34 -0600

    Estilos ArmarPlan

--- src/views/ArmarPlan.jsx
+++ src/views/ArmarPlan.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import fondiArmarPlan from "../imgs/piramid.jpg";
 import "../estilos/styArmarPlan.css";
 import FichaLugares from "../componentes/FichaLugares";
-import Lugar1 from '../imgs/explora01.jpg';
+import Lugar1 from "../imgs/explora01.jpg";
 import { Link } from "react-router-dom";
 
 export default function ArmarPlan() {
@@ -11,7 +11,7 @@ export default function ArmarPlan() {
   const [ciudad, setCiudad] = useState("");
   const [estancia, setEstancia] = useState("Unas cuantas horas");
   const [acompanantes, setAcompanantes] = useState("Voy solo");
-  const [presupuesto, setPresupuesto] = useState(2)
+  const [presupuesto, setPresupuesto] = useState(2);
   const handleExploraActividades = () => {
     navigate("/LugaresCarrusel", {
       state: { ciudad, estancia, acompanantes, presupuesto },
@@ -30,16 +30,19 @@ export default function ArmarPlan() {
           backgroundPosition: "center", // Centra la imagen
         }}
       >
-        <div className="contenedorDosDos">
+        <div className="contenedorDos">
+          <h1>Busquemos algo increíble</h1>
           <div className="contenidoImagenPlan">
-            <h1>Busquemos algo increíble</h1>
             <div className="selector-item">
-              <label>Agregar Ciudad</label>
-              <input
-                type="text"
+              <label>Buscar ciudad</label>
+              <select
+                className="select-field"
                 value={ciudad}
                 onChange={(e) => setCiudad(e.target.value)}
-              ></input>
+              >
+                <option>Ubicación actual</option>
+                <option>Ciudad de México</option>
+              </select>
             </div>
             <div className="selector-item">
               <label>Tiempo estimado de estancia</label>
@@ -76,36 +79,21 @@ export default function ArmarPlan() {
                 onChange={(e) => setPresupuesto(e.target.value)}
               />
             </div>
-            <button
-              className="search-button"
-              onClick={handleExploraActividades}
-            >
-              Explora actividades
-            </button>
             <div className="button-container"></div>
           </div>
+          <button className="botonAccion" onClick={handleExploraActividades}>
+            Explora actividades
+          </button>
         </div>
       </div>
 
-        {/* Sección de lugares recomendados */}
- <section className="categoriasSec">
+      {/* Sección de lugares recomendados */}
+      <section className="categoriasSec">
         <h2>Lugares Recomendados</h2>
         <div className="categorias">
-          <FichaLugares
-            titulo="Lugar1"
-            contenido=""
-            imagen={Lugar1}
-          />
-          <FichaLugares
-            titulo="Lugar2"
-            contenido=""
-            imagen={Lugar1}
-          />
-          <FichaLugares
-            titulo="Lugar3"
-            contenido=""
-            imagen={Lugar1}
-          />
+          <FichaLugares titulo="Lugar1" contenido="" imagen={Lugar1} />
+          <FichaLugares titulo="Lugar2" contenido="" imagen={Lugar1} />
+          <FichaLugares titulo="Lugar3" contenido="" imagen={Lugar1} />
         </div>
         <Link to="/Lugares" className="more-categories-link">
           Más lugares...
